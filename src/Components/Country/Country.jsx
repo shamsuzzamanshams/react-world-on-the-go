2c84d16962e97bd82f643c8721fd7126e1e7dd23
shamsuzzamanshams <shams30200@gmail.com>
2026-02-26 16:50:58 +0600

    conditional rendaring

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({ country }) => {
-	// console.log(country.region.region);
+	const [visited, setVisited] = useState(false)
+	// console.log(country.area.area);
+
+	const handelVisited = () => {
+		// setVisited(true);
+		setVisited(!visited);
+
+	}
 
 	return (
-		<div className='country'>
+		<div className={`country ${visited && 'country-visited'}`}>
 			<img src={country.flags.flags.png} alt={country.flags.flags.alt} />
 			<h3>Name: {country.name.common}</h3>
 			<p>Population: {country.population.population}</p>
 			<p>Region: {country.region.region}</p>
+			<p>Area: {country.area.area}
+				{country.area.area > 300000 ? " Big Country" : " Small Country"}
+			</p>
+			<button onClick={handelVisited}>
+				{visited ? 'Visited' : 'Not Visited'}
+			</button>
 		</div>
 	);
 };
